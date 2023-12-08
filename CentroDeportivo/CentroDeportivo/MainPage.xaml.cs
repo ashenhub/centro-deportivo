937454fc917b7fe6216659ef200584127ce53861
ashenhub <angelgl@usal.es>
2023-12-08 01:32:50 +0100

    Interfaz de la pantalla principal

---
+++ CentroDeportivo/CentroDeportivo/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CentroDeportivo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        async void AltaBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new AltaUsuarioPage());
+        }
+
+        async void RegistrarBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrarVisitaPage());
+        }
+
+        void ListadoBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void ListadoFechaBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void ModificarSocioBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void EliminarBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
+
